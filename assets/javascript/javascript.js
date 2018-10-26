dd76e953c0cead6f16bd91011ced5bcda829bfcb
Pete Gallagher <petegallagher@Peter.local>
2018-10-26 11:08:18 -0700

    Update

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,2 +1,59 @@
+var letters = ["a", "b", "c"]
+
+var guessedLetters = [];
+
+var letterToGuess = null;
+
+var guessesLeft = 10;
+
+var wins = 0;
+var losses= 0;
+
+var updateGuessesLeft = function(){
+    document.querySelector("#guesses-left").innerHTML = guessesLeft;
+};
+
+var updateLetterToGuess = function(){
+    letterToGuess = letters[Math.floor(Math.random()*letters.length)];
+};
+
+var updateGuessesSoFar = function(){
+    document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+};
+
+var reset = function(){
+    guessesLeft = 10;
+    guessedLetters = [];
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+};
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+document.onkeydown = function(event){
+    guessesLeft--;
 
 
+var letter = String.fromCharCode(event.which).toLowerCase();
+
+guessedLetters.push(letter);
+
+updateGuessesLeft();
+updateGuessesSoFar();
+
+if (letter === letterToGuess){
+    wins++;
+    document.querySelector("#wins").innerHTML = wins;
+    
+    reset();
+}
+
+if (guessesLeft === 0){
+    losses++;
+    document.querySelector("#losses").innerHTML = losses;
+
+    reset();
+}
+};
